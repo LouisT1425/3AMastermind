f0be0dc00c64875904ceea91c0463cfdd97441e3
Louis T <104893531+LouisT1425@users.noreply.github.com>
2023-09-12 11:11:15 +0200

    Added the guess function, which check if the input code has perfectly placed color, or if the input code has valid colors but not in the right place.

--- main.py
+++ main.py
@@ -8,6 +8,22 @@ def randoCode(number_of_color, color_array):
     return color_code
 
 
+def guess(choice, color_code):
+    good_guess = 0
+    wrong_placement = 0
+    for i in range(len(color_code)):
+        if choice[i] == color_code[i]:
+            good_guess += 1
+        for j in range(len(color_code)):
+            if choice[i] == color_code[j]:
+                wrong_placement += 1
+    return [good_guess, wrong_placement - good_guess]
+
+
 possible_colors = ['R', 'G', 'B', 'Y', 'P', 'W']
 code = randoCode(4, possible_colors)
 print("The code is : ", code)
+user = ['R', 'W', 'G', 'B']
+print("Your code is : ", user)
+results = guess(user, code)
+print("You have ", results[0], " good guesses and ", results[1], " wrong placement but right color.")
